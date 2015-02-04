dd8a6797d66fb4437f50e12403357bdbc5096a52
Kate Catlin <kate.catlin@detroitlabs.com>
2015-02-04 14:18:07 -0500

    Updating with better code

--- app/src/main/java/com/example/katecatlin/jsonpractice/Main.java
+++ app/src/main/java/com/example/katecatlin/jsonpractice/Main.java
@@ -14,6 +14,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -70,74 +71,73 @@ public class Main extends Activity {
             private final String NAME_OF_GOOD = "title";
             private final String RESULTS = "results";
             private final String DESCRIPTION = "description";
-            String etsyJsonString = "there was an error!";
+            String etsyJsonString = "If this is your string, there was an error!";
 
             @Override
             protected String[] doInBackground(Void... params) {
 
-                HttpURLConnection urlConnection = null;
-                BufferedReader reader = null;
-
+                //We only need to put two pieces together in this example, but in real coding you may have many more.
+                //If there are lots of parameters in your search, you'll have to adjust the string below and add even more. 
                 final String URL_BASE = "https://openapi.etsy.com/v2/listings/active?api_key=";
                 final String API_KEY = "8yuclbhlmoijgd6jld92l6t4";
-//        URL urlToUse = null;
+
+                //Declare the variables up here that you'll need in both the "try" and the "catch"
+                HttpURLConnection urlConnection = null;
+                InputStream inputStream = null;
+
 
                 try {
+                    //Concatenate the URL
                     String urlString = URL_BASE + API_KEY;
                     URL urlToUse = new URL(urlString);
-                    Log.d(LOG_TAG, "The URL string is " + urlString);
 
+                    //Connect to the URL
                     urlConnection = (HttpURLConnection) urlToUse.openConnection();
                     urlConnection.setRequestMethod("GET");
                     urlConnection.connect();
 
-                    // Read the input stream into a String
-                    InputStream inputStream = urlConnection.getInputStream();
-                    StringBuffer buffer = new StringBuffer();
-                    if (inputStream == null) {
-                        // Nothing to do.
-                        return null;
-                    }
-                    reader = new BufferedReader(new InputStreamReader(inputStream));
+                    // Get all the bytes of input from the inputStream
+                    inputStream = urlConnection.getInputStream();
 
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                        // But it does make debugging a *lot* easier if you print out the completed
-                        // buffer for debugging.
-                        buffer.append(line + "\n");
+
+                    //Put the bytes into an ultra-speedy reader to read faster!
+                    BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+                    //Read the bytes and add them to a special String type called "StringBuilder" one by one.
+                    int bytesRead;
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while ((bytesRead = bufferedInputStream.read()) != -1) {
+                        stringBuilder.append((char)bytesRead);
                     }
 
-                    if (buffer.length() == 0) {
-                        // Stream was empty.  No point in parsing.
-                        return null;
-                    }
-                    etsyJsonString = buffer.toString();
-                    Log.d(LOG_TAG, "EtsyJSONString is " + etsyJsonString);
+                    //We have a big long string of JSON words!
+                    etsyJsonString = stringBuilder.toString();
 
 
                 } catch (IOException e) {
-                    Log.e(LOG_TAG, "Error ", e);
-                    // If the code didn't successfully get the weather data, there's no point in attemping
-                    // to parse it.
+                    // Catch if the code didn't successfully get the weather data.
+                    Log.e(LOG_TAG, "IOException ", e);
                     return null;
                 } finally {
                     if (urlConnection != null) {
                         urlConnection.disconnect();
                     }
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (final IOException e) {
-                            Log.d(LOG_TAG, "Error closing stream", e);
+                    try {
+                        if (inputStream != null) {
+                            inputStream.close();
                         }
+                    } catch (IOException e) {
+                        Log.d(LOG_TAG, "IOException", e);
                     }
                 }
-                try {
-                    Log.d(LOG_TAG, "Ok so it's making it to the last line...");
-                    String[] theInfoYouWanted = parsePostingFromJsonString(etsyJsonString);
-                    return theInfoYouWanted;
 
+
+                //Now that we have the big long JSON string, we gotta parse it.
+                try {
+                    String[] theInfoYouWanted = parsePostingFromJsonString(etsyJsonString);
+                    //Once you get the two strings you want form the JSON string, return them! Returning from this ASYNC task
+                    //automatically sends you down to "onPostExecute".
+                    return theInfoYouWanted;
                 } catch (JSONException e) {
                     return null;
                 }
@@ -145,31 +145,36 @@ public class Main extends Activity {
 
 
             private String[] parsePostingFromJsonString(String etsyJsonString) throws JSONException {
-//                Log.d(LOG_TAG, "inside the last class, the string is... " + etsyJsonString);
                 String title;
                 String description;
+
+                //Create a JSONOBject from the JSON string
                 JSONObject postingJSONObject = new JSONObject(etsyJsonString);
+
+                //Get the Array from within the object called "RESULTS"
                 JSONArray arrayOfPostings = postingJSONObject.getJSONArray(RESULTS);
 
+                //Fetch the first JSONObject within the results array, which is the most recent posting.
                 JSONObject mostRecentPosting = arrayOfPostings.getJSONObject(0);
-//                Log.d(LOG_TAG, "mostRecentPosting is " + mostRecentPosting);
+
+                //Get the title and description of the most recent posting
                 title = mostRecentPosting.getString(NAME_OF_GOOD);
                 description = mostRecentPosting.getString(DESCRIPTION);
-                Log.d(LOG_TAG, "title at last is " + title);
-                Log.d(LOG_TAG, "description at last is " + description);
 
+                //Make an array out of the two strings you found, the name and the description.
                 String[] arrayOfTitleAndDescription = new String[2];
                 arrayOfTitleAndDescription[0] = title;
                 arrayOfTitleAndDescription[1] = description;
+
                 return arrayOfTitleAndDescription;
             }
 
             protected void onPostExecute(String[] result) {
                 if (result != null) {
-                    Log.d(LOG_TAG, "IT'S NOT NULL IN THE POST EXECUTE, MANNNN!");
+                    //Set the your textviews in the Activity to your new results!
+                    //In real coding, you'd have to return this info through an "interface", but that's a lesson for another day...
                     textView.setText(result[0]);
                     descriptionView.setText(result[1]);
-
                 }
 
             }
